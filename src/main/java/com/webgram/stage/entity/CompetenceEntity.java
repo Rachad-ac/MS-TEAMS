5b56032ba9c214e541522ad864ba5cd55e8bd8ed
Emilie Fall Cisse <miliecisse11@email.com>
2025-06-25 16:24:36 +0000

    crud Competence

---
+++ src/main/java/com/webgram/stage/entity/CompetenceEntity.java
@@ -0,0 +1,33 @@
+package com.webgram.stage.entity;
+
+import com.webgram.stage.entity.enums.NiveauCompetence;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "competence")
+@Entity
+public class CompetenceEntity implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nom_competence")
+	private String nom;
+
+	@Column(name = "niveau")
+	@Enumerated(EnumType.STRING)
+	private NiveauCompetence niveau;
+
+	@Column(name = "domaine_competence")
+	private String domaine;
+
+}
---
+++ src/main/java/com/webgram/stage/model/CompetenceDTO.java
@@ -0,0 +1,27 @@
+package com.webgram.stage.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CompetenceDTO implements Serializable {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+    @NotEmpty
+    private String nom;
+    private String niveau;
+    private String domaine;
+}
