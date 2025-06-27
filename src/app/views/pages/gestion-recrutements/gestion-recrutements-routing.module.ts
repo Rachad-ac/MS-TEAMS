b023310fc3c8c0bc1d15b9cff2249b8e478eaf4a
Baye Cheikh MBACKE <mbackebayecheikh1@gmail.com>
2025-06-27 17:08:17 +0000

    Candidature

--- src/app/views/pages/gestion-recrutements/gestion-recrutements-routing.module.ts
+++ src/app/views/pages/gestion-recrutements/gestion-recrutements-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ListRecrutementComponent} from "./recrutement/list-recrutement/list-recrutement.component";
-import {DetaillesRecrutementComponent} from "./recrutement/detailles-recrutement/detailles-recrutement.component";
+import { ListRecrutementComponent } from './recrutement/list-recrutement/list-recrutement.component';
+import { DetaillesRecrutementComponent } from './recrutement/detailles-recrutement/detailles-recrutement.component';
+import { ListCandidatureComponent } from './candidature/list-candidature/list-candidature.component';
+import { DetaillesCandidatureComponent } from './candidature/detailles-candidature/detailles-candidature.component';
 
+// Définition des routes pour la gestion des recrutements et candidatures
 const routes: Routes = [
-  {path: '', component: ListRecrutementComponent},
-  {path: 'recrutement', component: ListRecrutementComponent},
-  {path: 'recrutement/:id', component: DetaillesRecrutementComponent},
+  // Routes pour les recrutements
+  { path: '', component: ListRecrutementComponent },
+  { path: 'recrutement', component: ListRecrutementComponent },
+  { path: 'recrutement/:id', component: DetaillesRecrutementComponent },
+  // Routes pour les candidatures
+  { path: 'candidature', component: ListCandidatureComponent },
+  { path: 'candidature/:id', component: DetaillesCandidatureComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class GestionRecrutementsRoutingModule { }
+// Module de routing pour la gestion des recrutements et candidatures
+export class GestionRecrutementsRoutingModule {}
--- src/app/views/pages/gestion-recrutements/gestion-recrutements.module.ts
+++ src/app/views/pages/gestion-recrutements/gestion-recrutements.module.ts
@@ -1,21 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Import des modules de routing et des composants nécessaires
 import { GestionRecrutementsRoutingModule } from './gestion-recrutements-routing.module';
 import { ListRecrutementComponent } from './recrutement/list-recrutement/list-recrutement.component';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatTableModule} from "@angular/material/table";
-import {NgbDropdownModule, NgbNavModule, NgbPaginationModule} from "@ng-bootstrap/ng-bootstrap";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import {
+  NgbDropdownModule,
+  NgbNavModule,
+  NgbPaginationModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { DetaillesRecrutementComponent } from './recrutement/detailles-recrutement/detailles-recrutement.component';
-
+import { ListCandidatureComponent } from './candidature/list-candidature/list-candidature.component';
+import { DetaillesCandidatureComponent } from './candidature/detailles-candidature/detailles-candidature.component';
+import { AddEditCandidatureComponent } from './candidature/add-edit-candidature/add-edit-candidature.component';
 
 @NgModule({
+  // Déclaration de tous les composants utilisés dans ce module
   declarations: [
     ListRecrutementComponent,
     DetaillesRecrutementComponent,
+    ListCandidatureComponent,
+    DetaillesCandidatureComponent,
+    AddEditCandidatureComponent,
   ],
+  // Import des modules nécessaires pour ce module
   imports: [
     CommonModule,
     GestionRecrutementsRoutingModule,
@@ -25,7 +38,9 @@ import { DetaillesRecrutementComponent } from './recrutement/detailles-recruteme
     MatTableModule,
     NgbPaginationModule,
     NgbNavModule,
-    NgbDropdownModule
-  ]
+    NgbDropdownModule,
+    ReactiveFormsModule,
+  ],
 })
-export class GestionRecrutementsModule { }
+// Module principal pour la gestion des recrutements et candidatures
+export class GestionRecrutementsModule {}
