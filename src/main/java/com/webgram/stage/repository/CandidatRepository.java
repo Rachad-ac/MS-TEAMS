6a788b316c9969ec2962a8ef015fb353420f50ca
Marie Diouf SARR <mariediouf.sarr@unchk.edu.sn>
2025-06-30 01:11:51 +0000

    premier commit de la branch marie

---
+++ src/main/java/com/webgram/stage/repository/CandidatRepository.java
@@ -0,0 +1,9 @@
+package com.webgram.stage.repository;
+import com.webgram.stage.entity.CandidatEntity;
+import com.webgram.stage.entity.OffreEmploiEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface  CandidatRepository  extends JpaRepository<CandidatEntity, Long> , QuerydslPredicateExecutor<CandidatEntity> {
+}
